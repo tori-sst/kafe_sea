4192143951b12d26c520335efbbceca24648f7ec
Vik <viktoriastermer@gmail.com>
2026-04-20 10:36:41 +0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://tori-sst.github.io',
-  base: '/kafe_sea',
+  site: 'https://astro.vik.roibee.ru',
 });
